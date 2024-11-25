ec50bbfec654ef894783b7675b16bedaeef1c088
shikhilchawla <shikhilchawla@gmail.com>
2024-11-25 00:49:05 -0600

    Updated Application and Artifact Name

--- src/main/java/org/phoenix/apps/community/post/UserPostsApplication.java
+++ src/main/java/org/phoenix/apps/community/post/UserPostsApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CommunityPostApplication {
+public class UserPostsApplication {
     public static void main (String[] args){
-        SpringApplication.run(CommunityPostApplication.class);
+        SpringApplication.run(UserPostsApplication.class);
     }
 }
